4086bd57fd0f001aee87b227068bb0a4c4b42c34
lhhboy <1603614324@qq.com>
2024-04-10 00:15:16 +0800

    feat:对scss进行配置，定义全局css变量，登录界面样式和favicon.ico

---
+++ src/views/login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="login-bg">
+    <div class="login-form">登录界面</div>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style  lang="scss" scoped>
+.login-bg {
+  width: 100%;
+  margin: 0px;
+  height: 100%;
+  background: url("@/assets/images/login-bg.jpg") no-repeat;
+  background-size: 100% 100%;
+  background-attachment: fixed;
+  .login-form {
+    width: 600px;
+    height: 400px;
+    background-color: red;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+    border-radius: $dialog-radius-large;
+  }
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import path from "path";
 export default defineConfig({
-  plugins: [vue()],
-})
+  // ...
+  plugins: [
+    [vue()],
+    // ...
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      // "@store": path.resolve(__dirname, "./src/store"),
+      // "@api": path.resolve(__dirname, "./src/api"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/index.scss";`,
+      },
+    },
+  },
+});
